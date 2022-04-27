5a3c38910759e7e1927561430a1cd554d8557ea6
mraheeldev <m.raheeldev@gmail.com>
2022-04-27 12:07:43 +0500

    dev_done

--- app/src/main/java/com/example/imagepickerold/MainActivity.kt
+++ app/src/main/java/com/example/imagepickerold/MainActivity.kt
@@ -5,18 +5,22 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.ImageDecoder
 import android.net.Uri
-import android.os.Build
-import android.os.Bundle
-import android.os.ParcelFileDescriptor
+import android.os.*
 import android.provider.MediaStore
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.imagepickerold.databinding.ActivityMainBinding
 import java.io.FileDescriptor
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    val PICK_IMAGE = 1
+    private val workerHandler = Handler(Looper.getMainLooper())
+    private val workerThread: ExecutorService = Executors.newCachedThreadPool()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,14 +28,36 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.imagePicker.setOnClickListener {
-
-            val pickPhoto = Intent(
-                Intent.ACTION_PICK,
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI
-            )
-            @Suppress("DEPRECATION")
-            startActivityForResult(pickPhoto, 1)
+            threeWayToPickerUpImage()
         }
+    }
+
+    //But in some devices, while setting the intent type, the above solution will clear the intent data (MediaStore.Images.Media.EXTERNAL_CONTENT_URI) which could hinder the gallery opening process.
+    @Suppress("DEPRECATION")
+    private fun oneWayToPickerUpImage() {
+        val pickPhoto = Intent(
+            Intent.ACTION_PICK,
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        )
+        startActivityForResult(pickPhoto, PICK_IMAGE)
+    }
+
+    //But in some devices, the above solution will not fetch the image with EXIF information such as orientation. So in those devices, EXIF processing such as changing image orientation could not be performed as expected.
+    @Suppress("DEPRECATION")
+    private fun twoWayToPickerUpImage() {
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE)
+    }
+
+    //Finally, I suggest the below code which allows user to select an image from any gallery application which does not cause any problem and does not show up any warning
+    @Suppress("DEPRECATION")
+    private fun threeWayToPickerUpImage() {
+        val intent = Intent()
+        intent.action = Intent.ACTION_PICK
+        intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*")
+        startActivityForResult(intent, PICK_IMAGE)
     }
 
     @Deprecated("Deprecated in Java")
@@ -39,20 +65,35 @@ class MainActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if (data != null && resultCode == RESULT_OK) {
+        if (data != null && resultCode == RESULT_OK && requestCode == PICK_IMAGE) {
 
             Toast.makeText(this, "data is not null", Toast.LENGTH_SHORT).show()
 
             val uri = data.data
 
             if (uri != null) {
-
-                getBitmap(uri)?.let {
-                    binding.imageView.setImageBitmap(it)
-                }
+                setImageAsyncTask(uri)
             }
 
         }
+    }
+
+    private fun setImageAsyncTask(uri: Uri) {
+
+        workerThread.execute {
+            val bitmap = getBitmap(uri)
+            bitmap?.let {
+                workerHandler.post {
+                    binding.imageView.setImageBitmap(it)
+                }
+            }
+        }
+
+        /*workerHandler.post {
+            getBitmap(uri)?.let {
+                binding.imageView.setImageBitmap(it)
+            }
+        }*/
     }
 
     private fun getBitmapFromContentResolver(shareUri: Uri): Bitmap? {
